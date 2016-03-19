3ef9060d8b749cd8ad18bf96ab9794874733452a
Iván García <ciberpatro@gmail.com>
2016-03-19 19:10:07 +0100

    Implementado algoritmo Forward

--- NIMGame/src/algorithms/Forward.java
+++ NIMGame/src/algorithms/Forward.java
@@ -1,12 +1,47 @@
 package algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import States.State;
 import States.StateSpace;
 
 public class Forward {
-	public static List<Integer> getNextMove(StateSpace stateSpace, List<Integer>  initialState){
-		
-		return null;
+	private StateSpace stateSpace;
+	public Forward(State baseState){
+		stateSpace=new StateSpace(baseState);
+	}
+	public State getNextMove(State baseState){
+		int auxVal, val=0;
+		boolean found=false;
+		List<State> stateList=new ArrayList<State>();
+		List<State> ant=new ArrayList<State>();
+		State ini=new State(baseState.len());
+		State mov=null;
+		baseState.sort();
+		stateSpace.setLose(ini);
+		stateList.add(ini);
+		while (!stateList.isEmpty() && !found) {
+			State s = stateList.remove(0);
+			val = stateSpace.getVal(s);
+			ant = stateSpace.anteccessors(s, baseState);
+			if (ant.contains(baseState)) {
+				mov = s;
+				if (val == -1) {
+					found = true;
+				}
+			} else {
+				for (State next : ant) {
+					auxVal = stateSpace.getVal(next);
+					if (val == 1 && auxVal == 0) {
+						stateSpace.setLose(next);
+					} else if (val == -1 && (auxVal == 0 || auxVal == -1)) {
+						stateSpace.setWin(next);
+					}
+				}
+			}
+			stateList.addAll(ant);
+		}
+		return mov;
 	}
 }
